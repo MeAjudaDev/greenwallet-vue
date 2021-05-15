93b60ba3551f3428cf43a8e9c599303f98f82f50
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-05-15 16:39:06 -0300

    RF23 - Criacao do header da tela de configuracoes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  Hello World
+  <header>
+    <div id="header">
+      <div id="btn-avatar">
+        <div id="btn"></div>
+        <div id="avatar"></div>
+      </div>
+      <h2>Configurações</h2>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -11,6 +19,41 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Roboto;
+  font-weight: normal;
+}
+
+body {
+  width: 100vw;  
+}
+
+#header {
+  background-color: #01C9B8;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  height: 9rem;
+  padding: 25px 20px;
+}
+
+#btn-avatar {  
+  display: flex;  
+  width: 100%;
+  height: 2rem;    
+}
+
+h2 {
+  width: 100%;
+  text-align: center;
+  font-size: 25px;
+  color: #FFFFFF;   
+}
 
 </style>
